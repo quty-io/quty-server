0f5937335a1736539bcf366cb44943b36169efd4
snupa <adrian@unloq.io>
2019-04-19 13:52:31 +0300

    - added initial quty.Publisher functionality - added ping/pong termination checker for client/server

---
+++ example/quty-cluster.js
@@ -0,0 +1,5 @@
+'use strict';
+/**
+ * This example shows a simple Quty cluster and a separate Quty Client that connects to the cluster
+ * to send messages only (it does not process messages, just publish messages)
+ * */
